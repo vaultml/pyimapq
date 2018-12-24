8517bffe64e39bb11178ed592ff491bc76eb1f7c
Nir Tzachar <nir.tzachar@gmail.com>
2018-12-24 17:06:11 +0200

    minor name change

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pymailq",
+    name="pyimapq",
     version="0.0.1",
     author="Nir Tzachar",
     author_email="nir.tzachar@gmail.com",
     description="An IMAP queue package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/vaultml/pymailq",
+    url="https://github.com/vaultml/pyimapq",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
